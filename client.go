4a7a9a0c5ce7c1a05065f924bc3279b37c2d4583
chandrafebriantvlk99 <chandra.febrian@traveloka.com>
2025-05-17 17:33:18 +0700

    chandra server-client using protobuf-grpc with golang

---
+++ client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"chandra-grpc-protobuff-golang/userpb"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("Could not connect:", err)
+	}
+	defer conn.Close()
+
+	client := userpb.NewUserServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	req := &userpb.UserRequest{UserId: "889"}
+	res, err := client.GetUser(ctx, req)
+	if err != nil {
+		log.Fatalf("Error when calling GetUser: %v", err)
+	}
+
+	log.Printf("Response: Name=%s, Age=%d", res.GetName(), res.GetAge())
+}
---
+++ server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"chandra-grpc-protobuff-golang/userpb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	userpb.UnimplementedUserServiceServer
+}
+
+func (s *server) GetUser(ctx context.Context, req *userpb.UserRequest) (*userpb.UserResponse, error) {
+	log.Printf("Received request for user_id: %s\n", req.GetUserId())
+	return &userpb.UserResponse{
+		Name: "Chandra",
+		Age:  38,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := grpc.NewServer()
+	userpb.RegisterUserServiceServer(s, &server{})
+
+	fmt.Println("Server is running on port 50051...")
+	if err := s.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
+}
